accb90a3f6edc06b2dfa9d7bc1dda177f28276ba
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2015-12-05 20:54:02 +0100

    First commit

---
+++ js/js.js
@@ -0,0 +1,18 @@
+var player 1, player 2 = 
+
+
+// create a board
+
+
+div id = gameBoard
+
+
+//  cambiare pulsante con anatra
+
+input type  button e cambio immagine con anatra
+
+// aggiungere punteggio score
+
+
+
+//
