c466154c00f84f14a11698ee8cbf59a5cbea20b1
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2015-12-05 21:29:11 +0100

    Small changes

--- js/js.js
+++ js/js.js
@@ -1,18 +1,2 @@
-var player 1, player 2 = 
-
-
-// create a board
-
-
-div id = gameBoard
-
-
-//  cambiare pulsante con anatra
-
-input type  button e cambio immagine con anatra
-
-// aggiungere punteggio score
-
-
-
-//
+var playerName = prompt("What's your name soldier?", ".........");
+document.write("Hey " + playerName + "let's go");
