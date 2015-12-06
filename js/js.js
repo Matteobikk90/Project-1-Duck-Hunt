ab405d4d15521a526b05c383b6e1ce99d4f9db95
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2015-12-06 17:15:22 +0100

    Third commit

--- js/js.js
+++ js/js.js
@@ -1,2 +1,21 @@
-var playerName = prompt("What's your name soldier?", ".........");
-document.write("Hey " + playerName + "let's go");
+alert('uhuh');
+
+/*soundManager.setup({                                                                                    //configure soundManager
+  url: '/swf',
+  onready: function() {
+      setUpSoundBoard()                                                                                 //onReady, call setUpSoundBoard() function
+  }
+});
+*/
+
+($(document).ready(function() {  
+(".duckbtn").click(function() { 
+    console.log("okkkkkk")
+})
+
+
+
+//var playerName = prompt("What's your name soldier?", ".........");
+//document.write("Okey " + playerName + " let's go");
+
+});
