0630dc8807de0427ba86825aa33e3df11b0ad745
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2015-12-06 19:56:12 +0100

    Several changes + score counter

--- js/js.js
+++ js/js.js
@@ -1,40 +1,41 @@
 $(document).ready(function() { 
+	console.log("hello");
+
+var counter = 0;
 
 $(".duckbtn").click(function() { 
-
-    console.log("okkkkkk")
+	counter+= 5;
+	console.log(counter);
+    $("#score").html(counter);
 });
-
-soundManager.setup({
-   url: 'js/soundmanager/swf',
-   flashVersion: 9, 
-   onready: function() {
-
-   console.log("soundmanager");
+});
+//soundManager.setup({
+//   url: 'js/soundmanager/swf',
+//   flashVersion: 9, 
+//   onready: function() {
+//
+//   console.log("soundmanager");
 
 
      
-var playButton = document.getElementsByClassName("duckbtn");
-  playButton[0].addEventListener("click" , function(event) {
-    var mySound = soundManager.createSound({
-      "id": 'duck',
-      "url" :"js/sounds/quack.aiff",
-    });
-    mySound.play(); 
-  })
+//var playButton = document.getElementsByClassName("duckbtn");
+// playButton[0].addEventListener("click" , function(event) {
+//   var mySound = soundManager.createSound({
+//     "id": 'duck',
+//     "url" :"js/sounds/quack.aiff",
+//   });
+//   mySound.play(); 
+// })
 
-};
+//}
 
-$(document).ready(function() { 
 
-$(".duckbtn").click(function() { 
-
-    console.log("okkkkkk")
-});
 
 
 
 //var playerName = prompt("What's your name soldier?", ".........");
 //document.write("Okey " + playerName + " let's go");
 
-});
+
+// $(".duckbtn").animate({left: "100px", top: "240px"});
+
